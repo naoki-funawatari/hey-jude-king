b647e548abc812f768d690dc3cc1d5ea67c25e66
naoki-funawatari <g011c1180@gmail.com>
2022-05-02 21:29:03 +0900

    chore: パッケージを最新化。

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import * as ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "@/features/App";
 
-ReactDOM.render(
-  <Router>
-    <RecoilRoot>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <RecoilRoot>
+    <Router>
       <App />
-    </RecoilRoot>
-  </Router>,
-  document.getElementById("root")
+    </Router>
+  </RecoilRoot>
 );
